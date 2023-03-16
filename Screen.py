74e50022d766aa5ff2695a0c8f02ed26d12ecc87
ScottMinalga <115512109+ScottMinalga@users.noreply.github.com>
2023-03-16 17:06:33 -0400

    eod
    eod

--- Screen.py
+++ Screen.py
@@ -1,48 +1,106 @@
-from flask import Flask, request, flash, url_for, redirect, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask (__name__)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects/sql.db'
+#create instance of the app
+app = Flask(__name__)
+app.secret_key = "marubeni"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHENY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime = timedelta(minutes=120)
+
 
 db = SQLAlchemy(app)
 
+#model for SQL
 class projects(db.Model):
-    id = db.Column('database_id' , db.Integer, primary_key = True)
-    projectnum = db.Column(db.String(50))
-    jobnum = db.Column(db.String(50))
-    salesnum = db.Column(db.String(50))
-    customername = db.Column(db.String(100))
-    builder = db.Column(db.String(50))
-    status = db.Column(db.String(50))
+    _id = db.Column("id", db.Integer, primary_key=True)
+    project_number = db.Column(db.String(100))
+    job_number = db.Column(db.String(100))
+    sales_order = db.Column(db.String(100))
+    customer_name = db.Column(db.String(100))
+    builder = db.Column(db.String(100))
+    status = db.Column(db.String(100))
+    notes = db.Column(db.String(500))
 
-    def __init__(self, projectnum, jobnum, salesnum, customername, builder, status):
-        self.projectnum = projectnum
-        self.jobnum = jobnum
-        self.salesnum = salesnum
-        self.customername = customername
+
+
+    def __init__(self, project_number, job_number, sales_order, customer_name, builder, status, notes):
+        self.project_number = project_number
+        self.job_number = job_number
+        self.sales_order = sales_order
+        self.customer_name = customer_name
         self.builder = builder
         self.status = status
+        self.notes = notes
 
-@app.route('/')
-def table():
-    return render_template('show_all.html')
 
-@app.route('/new', methods = ['GET', 'POST'])
-def new():
+# we can use html to pass variable through
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
     if request.method == "POST":
-        if not request.form['projectnum'] or not request.form['jobnum'] or not request.form['salesnum'] or not request.form['customername']:
-            flash(f'Please enter all the fields----ERROR')
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
         else:
-            project = projects(request.form['projectnum'], request.form['jobnum'],
-                request.form['salesnum'], request.form['customername'], request.form['builder'], request.form['status'])
-
-            db.session.add(project)
+            usr = users(user,"")
+            db.session.add(usr)
             db.session.commit()
-            flash(f"Project was successfully added")
-            return redirect(url_for('table'))
-    return render_template('new.html')
 
-with app.app_context():
-    db.create_all()
+        flash("Login Succesful")
+        return redirect(url_for("user"))       
+    else:
+        if "user" in session:
+            flash("Already logged in!")
+            return redirect(url_for("user"))
+        
+        return render_template("login.html")
+
+@app.route("/user", methods=["POST", "GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email was saved!")
+        else:
+            if "email" in session:
+                email = session["email"]
+
+        return render_template("user.html", email=email)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user}!", "info")
+    session.pop("user", None)
+    session.pop("email", None)
+    return redirect(url_for("login"))
+
+#run the app and creats db database if not already done
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug = True)
-
