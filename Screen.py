aeb34d0f8f85f0bb3272e6e844b42b17a91606cf
Scott Minalga <115512109+ScottMinalga@users.noreply.github.com>
2023-05-02 09:26:37 -0400

    fixing table
    fixing table

--- Screen.py
+++ Screen.py
@@ -441,7 +441,7 @@ def login():
 
         user = User.query.filter_by(username=username).first()
         if user and user.check_password(password):
-            session['user_id'] = user._id
+            session['user_id'] = user.id
             session['username'] = user.username
             session['is_admin'] = user.is_admin
 
